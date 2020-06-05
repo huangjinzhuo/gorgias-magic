fb0a46530973845cbff60d9c2f018cf4d2a1a9b7
Jin Huang <huangjinzhuo@gmail.com>
2020-06-05 01:47:30 -0700

    Gorgias Magic Application in Python

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
-# from flask.ext.sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+'''Add and list todo list'''
 import os
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+# from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ db_pswd = os.environ.get('POSTGRES_DB_PSWD')
 db_host = os.environ.get('SERVICE_POSTGRES_SERVICE_HOST')
 
 # disregard warning messages
-app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False 
+app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False
 # database location and name of database (launchpage)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{0}:{1}@{2}/launchpage'.format(
     db_user, db_pswd, db_host
@@ -23,6 +24,7 @@ db = SQLAlchemy(app)
 
 # Create a class represent a table in the database model. The model map to a table.
 class Todo(db.Model):
+    '''define todo'''
     __tablename__ = "Todo"
     id = db.Column(db.Integer, primary_key=True)
     todo = db.Column(db.String(500), default='')
@@ -34,6 +36,7 @@ class Todo(db.Model):
 # add todo items: hostname/<todo_description>/<days_before_expired>
 @app.route('/<todo>/<days>')
 def index(todo):
+    '''add a todo item'''
     todo = Todo(todo=todo)
     days = Todo(days=days)
     db.session.add(todo)
@@ -44,13 +47,14 @@ def index(todo):
 
 # @app.route('/<todo>')
 # def get_todo(todo):
-#     todo = Todo.query.filter_by(todo=todo).first()
+# todo = Todo.query.filter_by(todo=todo).first()
 
 #     return f'{ todo.todo } expires in { todo.days }'
 
 
 @app.route('/')
 def get_todo(todo):
+    '''get todo list'''
     todo = Todo.query.all()
 
     return f'{ todo.todo } expires in { todo.days } from { todo.date_created }'
